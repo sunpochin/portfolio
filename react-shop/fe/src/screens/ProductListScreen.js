cc60667bdaaea6bbadd20dc23b8472b6b2992f62
sun pochin <sunpochin@gmail.com>
2022-01-23 19:48:29 +0800

    done product list

---
+++ react-shop/fe/src/screens/ProductListScreen.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+import products from '../products';
+
+const ProductListScreen = () => {
+	return (
+		<>
+			<Container>
+				<h1>Latest products</h1>
+				<Row>
+					{products.map((product) => (
+						<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+							<h3>{product.name}</h3>
+						</Col>
+					))}
+				</Row>
+			</Container>
+		</>
+	);
+};
+
+// {products.map(product => (
+//   <h3>{product.name}</h3>
+// ))}
+
+export default ProductListScreen;
